24061bf4d2110fb50e2a1a10195027d2e840003b
ubuntu <ubuntu@ubuntu-Lenovo-ideapad-330-15IKB>
2020-11-18 10:38:33 +0530

    sac

--- src/main/java/com/ats/ecommerce/common/Constants.java
+++ src/main/java/com/ats/ecommerce/common/Constants.java
@@ -4,9 +4,9 @@ package com.ats.ecommerce.common;
 //Desc - Constants fields
 
 public class Constants {
-	public static final String JSON_FILES_PATH="/home/ubuntu/Documents/apache-tomcat-8.51.38/webapps/IMG_UP/";
-	public static final String CAT_IMG_VIEW_URL="http://localhost:8080/IMG_UP/";
-	public static final String PROD_IMG_VIEW_URL="http://localhost:8080/IMG_UP/";
+	public static final String JSON_FILES_PATH="/opt/apache-tomcat-8.5.39/webapps/PROD_IMG_UP/";
+	public static final String CAT_IMG_VIEW_URL="http://198.12.156.31:8080/PROD_IMG_UP/";
+	public static final String PROD_IMG_VIEW_URL="http://198.12.156.31:8080/PROD_IMG_UP/";
 
 	
 }
