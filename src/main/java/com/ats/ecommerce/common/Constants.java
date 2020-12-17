c6160521be482387f5e6b103090d9f4d55b061d9
ubuntu <ubuntu@ubuntu-Lenovo-ideapad-330-15IKB>
2020-12-17 19:40:42 +0530

    Ecom-sac

--- src/main/java/com/ats/ecommerce/common/Constants.java
+++ src/main/java/com/ats/ecommerce/common/Constants.java
@@ -16,13 +16,21 @@ public class Constants {
 	 */
 
 	// Sachin Local Path
-	private final static String BASEPATH_URL="/home/ubuntu/Documents/apache-tomcat-8.51.38/webapps/";
-	private final static String IP_URL ="http://192.168.2.2:8080/";
+	/*
+	 * private final static String
+	 * BASEPATH_URL="/home/ubuntu/Documents/apache-tomcat-8.51.38/webapps/"; private
+	 * final static String IP_URL ="http://192.168.2.14:8080/";
+	 */
+	
+	
+	private final static String BASEPATH_URL="/opt/apache-tomcat-8.5.39/webapps/";
+	private final static String IP_URL ="http://198.12.156.31:8080/";
 
+	
 	public static final String JSON_FILES_PATH = BASEPATH_URL+"IMG_UP/";
 
-	public static final String CAT_IMG_VIEW_URL = IP_URL+"IMG_UP/";
-	public static final String PROD_IMG_VIEW_URL = IP_URL+"IMG_UP/";
+	public static final String CAT_IMG_VIEW_URL = IP_URL+"OFFER_IMG_UP/";
+	public static final String PROD_IMG_VIEW_URL = IP_URL+"PROD_IMG_UP/";
 	public static final String TESTMON_IMG_VIEW_URL = IP_URL+"IMG_UP/";
 
 	public static final String UPLOAD_URL = BASEPATH_URL+"IMG_UP/";
@@ -35,7 +43,9 @@ public class Constants {
 
 	public static RestTemplate rest = new RestTemplate();
 
-	public static String url="http://localhost:8094/";
+	//public static String url="http://localhost:8094/";
+	 public static final String url ="http://198.12.156.31:8080/EcomAPI/";
+
 
 	public static String[] imageFileExtensions={"jpg","jpeg","png"};
 
